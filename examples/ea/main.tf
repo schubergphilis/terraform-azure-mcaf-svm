32f6cf197f6ed52df8146f7e0360bd2f788adcec
Dennis van der Meulen <dvandermeulen@schubergphilis.com>
2024-10-22 14:49:12 +0200

    fix terraform format

--- examples/ea/main.tf
+++ examples/ea/main.tf
@@ -7,7 +7,7 @@ module "ea_subscription" {
   name                 = "sub-es-test"
   channel              = "ea"
   sku                  = "Production"
-  tags                 = {"tag1" = "value"}
+  tags                 = { "tag1" = "value" }
   owner_id             = "id"
   billing_account_name = "Billing Account Name"
   billing_profile_name = "Billing Profile Name"
