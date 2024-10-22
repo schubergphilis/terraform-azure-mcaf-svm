b693be0bfa75440dd7631804d67aeaa69cce6a67
Dennis van der Meulen <dvandermeulen@schubergphilis.com>
2024-10-22 14:44:19 +0200

    add examples

---
+++ examples/ea/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.7"
+}
+
+module "ea_subscription" {
+  source               = "../.."
+  name                 = "sub-es-test"
+  channel              = "ea"
+  sku                  = "Production"
+  tags                 = {"tag1" = "value"}
+  owner_id             = "id"
+  billing_account_name = "Billing Account Name"
+  billing_profile_name = "Billing Profile Name"
+  invoice_section_name = "Invoice Section Name"
+}
