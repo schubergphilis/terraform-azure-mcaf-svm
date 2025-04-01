d78a534230c0690815846542bffb9ae27f8edd84
fblankendaal <fblankendaal@schubergphilis.com>
2025-04-01 13:22:16 +0200

    fix resource name issue

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 output "subscription_id" {
-  value       = data.azapi_resource.subscription_metadata.output.subscriptionId[0]
+  value       = data.azapi_resource_list.subscription_metadata.output.subscriptionId[0]
   description = "subscription id"
 }
 
 output "display_name" {
-  value       = data.azapi_resource.subscription_metadata.output.displayName[0]
+  value       = data.azapi_resource_list.subscription_metadata.output.displayName[0]
   description = "subscription display name"
 }
 
 output "id" {
-  value       = data.azapi_resource.subscription_metadata.output.id[0]
+  value       = data.azapi_resource_list.subscription_metadata.output.id[0]
   description = "combined into an azure valid resource id"
 }
