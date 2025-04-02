a19bf92b5ee06db5f931fa72dfafeefb4becae35
fblankendaal <fblankendaal@schubergphilis.com>
2025-04-02 10:45:26 +0200

    output null if nothing gets returned

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 output "subscription_id" {
-  value       = data.azapi_resource_list.subscription_metadata.output.subscriptionId[0]
+  value       = length(data.azapi_resource_list.subscription_metadata.output.subscriptionId) > 0 ? data.azapi_resource_list.subscription_metadata.output.subscriptionId[0] : null
   description = "subscription id"
 }
 
 output "display_name" {
-  value       = data.azapi_resource_list.subscription_metadata.output.displayName[0]
+  value       = length(data.azapi_resource_list.subscription_metadata.output.displayName) > 0 ? data.azapi_resource_list.subscription_metadata.output.displayName[0] : null
   description = "subscription display name"
 }
 
 output "id" {
-  value       = data.azapi_resource_list.subscription_metadata.output.id[0]
+  value       = length(data.azapi_resource_list.subscription_metadata.output.id) > 0 ? data.azapi_resource_list.subscription_metadata.output.id[0] : null
   description = "combined into an azure valid resource id"
 }
